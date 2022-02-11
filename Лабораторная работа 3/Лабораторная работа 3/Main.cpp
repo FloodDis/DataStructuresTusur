3ffcfa8155590e64ebbe639e8dea9a9f508f08be
FloodDis <vlad.sechenov@mail.ru>
2022-02-11 18:05:37 +0700

    Исправил некоторые printf на cout

--- Лабораторная работа 3/Лабораторная работа 3/Main.cpp
+++ Лабораторная работа 3/Лабораторная работа 3/Main.cpp
@@ -283,23 +283,23 @@ void QueueRingBufferBasedMenu()
 	setlocale(LC_ALL, "Russian");
 	int option;
 	int sizeOfQueue;
-	printf("Введите размер очереди: ");
+	cout << "Введите размер очереди: ";
 	cin >> sizeOfQueue;
 	if ((int)log2(sizeOfQueue) % 2 != 0)
 	{
-		printf("Размер буфера должен степенью 2!\n");
+		cout << "Размер буфера должен степенью 2!\n";
 		return;
 	}
 	else
 	{
 		QueueRingBufferBased* queueRingBuffer = new QueueRingBufferBased;
 		queueRingBuffer->ringBuffer = CreationOfQueue(sizeOfQueue);
-		printf("Очередь создана!\n");
+		cout << "Очередь создана!\n";
 		ShowQueueRingBuffer(queueRingBuffer->ringBuffer);
 		while (true)
 		{
-			printf("Выберете вариант:\n1) Добавить элемент в очередь\n2) Извлечение элемента из очереди\n");
-			printf("3) Удалить очередь\n4) Выход\n");
+			cout << "Выберете вариант:\n1) Добавить элемент в очередь\n2) Извлечение элемента из очереди\n";
+			cout << "3) Удалить очередь\n4) Выход\n";
 			cin >> option;
 			switch (option)
 			{
@@ -315,14 +315,14 @@ void QueueRingBufferBasedMenu()
 			{
 				int elementFromQueue;
 				elementFromQueue = DequeueRingBuffer(queueRingBuffer->ringBuffer);
-				printf("Извлеченный элемент: %i\n", elementFromQueue);
+				cout << "Извлеченный элемент: %i\n", elementFromQueue;
 				ShowQueueRingBuffer(queueRingBuffer->ringBuffer);
 				break;
 			}
 			case 3:
 			{
 				DeleteQueueRingBuffer(queueRingBuffer->ringBuffer);
-				printf("Ваша очередь удалена!\n");
+				cout << "Ваша очередь удалена!\n";
 				int sizeOfQueue;
 				EnterNumber("Введите размер очереди: ", sizeOfQueue);
 				if ((int)log2(sizeOfQueue) % 2 != 0)
@@ -334,7 +334,7 @@ void QueueRingBufferBasedMenu()
 				else
 				{
 					queueRingBuffer->ringBuffer = CreationOfQueue(sizeOfQueue);
-					printf("Очередь создана!\n");
+					cout << "Очередь создана!\n";
 					break;
 				}
 			case 4:
@@ -344,7 +344,7 @@ void QueueRingBufferBasedMenu()
 			}
 			default:
 			{
-				printf("Попробуйте ещё раз!\n");
+				cout << "Попробуйте ещё раз!\n";
 				break;
 			}
 			}

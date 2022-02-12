fbaafd8c29fa794f45de501ba7dff0a2a6a1176e
FloodDis <vlad.sechenov@mail.ru>
2022-02-12 14:46:19 +0700

    Исправил код по правилам RSDN

--- Лабораторная работа 6/Лабораторная работа 6/Main.cpp
+++ Лабораторная работа 6/Лабораторная работа 6/Main.cpp
@@ -10,10 +10,24 @@ void main()
 
 void MainMenu()
 {
+	/// <summary>
+	/// Варианты выбора структуры данных
+	/// </summary>
 	enum Actions
 	{
+		/// <summary>
+		/// АВЛ-дерево
+		/// </summary>
 		AVLTree = 1,
+
+		/// <summary>
+		/// Красно-черное дерево
+		/// </summary>
 		RedBlackTree = 2,
+
+		/// <summary>
+		/// Выход из программы
+		/// </summary>
 		Exit = 3
 	};
 	setlocale(LC_ALL, "ru");

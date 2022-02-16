5f0773247b026af15aaffaa1e5411ab27f93dacf
FloodDis <vlad.sechenov@mail.ru>
2022-02-16 20:49:23 +0700

    Исправил код по правилам RSDN

--- Лабораторная работа 1/Лабораторная работа 1/Main.cpp
+++ Лабораторная работа 1/Лабораторная работа 1/Main.cpp
@@ -222,9 +222,9 @@ void ShowArray(StructOfArray* arrayUnit)
 
 void Menu(StructOfArray* arrayUnit)
 {
-	int option;
 	while (true)
 	{
+		int option;
 		cout << "Select an action:\n1) Add element\n2) Delete element\n";
 		cout << "3) Sort the array\n4) Search element\n5) Print the array\n";
 		cout << "6) Quit\n";

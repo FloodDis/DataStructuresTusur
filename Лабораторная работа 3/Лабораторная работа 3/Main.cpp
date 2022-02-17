ba9d880191b97435d24922b8032ed494d1bd80f1
FloodDis <vlad.sechenov@mail.ru>
2022-02-17 22:57:01 +0700

    fix(Лабораторная работа 3, Main): убрал условие для размера кольцевого буфера

--- Лабораторная работа 3/Лабораторная работа 3/Main.cpp
+++ Лабораторная работа 3/Лабораторная работа 3/Main.cpp
@@ -139,91 +139,83 @@ void RingBufferMenu()
 	int option;
 	int sizeOfBuffer;
 	EnterNumber("Введите размер буфера: ", sizeOfBuffer);
-	if ((int)log2(sizeOfBuffer) % 2 != 0)
+	RingBuffer* ringBuffer = InitializationOfRingBuffer(sizeOfBuffer);
+	printf("Буфер создан!\n");
+	while (true)
 	{
-		printf("Размер буфера должен быть степенью двойки!\n");
-		return;
-	}
-	else
-	{
-		RingBuffer* ringBuffer = InitializationOfRingBuffer(sizeOfBuffer);
-		printf("Буфер создан!\n");
-		while (true)
+		printf("Выберите вариант:\n1) Проверить, есть ли в буфере место для записи\n");
+		printf("2) Проверить, пуст ли буфер\n3) Вставить\n4) Взять\n5) Выход\n");
+		cin >> option;
+		switch (option)
 		{
-			printf("Выберите вариант:\n1) Проверить, есть ли в буфере место для записи\n");
-			printf("2) Проверить, пуст ли буфер\n3) Вставить\n4) Взять\n5) Выход\n");
-			cin >> option;
-			switch (option)
+			case 1:
 			{
-				case 1:
+				bool answer = IfThereSpaceToWrite(ringBuffer);
+				if (answer == true)
 				{
-					bool answer = IfThereSpaceToWrite(ringBuffer);
-					if (answer == true)
-					{
-						printf("В буфере есть место для записи!\n");
-					}
-					else
-					{
-						printf("В буфере нет места для записи!\n");
-					}
+					printf("В буфере есть место для записи!\n");
+				}
+				else
+				{
+					printf("В буфере нет места для записи!\n");
+				}
+				break;
+			}
+
+			case 2:
+			{
+				bool answer = IsEmpty(ringBuffer);
+				if (answer == true)
+				{
+					printf("\nБуфер пуст!\n");
+				}
+				else
+				{
+					printf("\nБуфер не пуст!\n");
+					ShowRingBuffer(ringBuffer);
+				}
+				break;
+			}
+
+			case 3:
+			{
+				if (IfThereSpaceToWrite(ringBuffer))
+				{
+					int newElement;
+					EnterNumber("Введите значение нового элемента: ", newElement);
+					PushInRingBuffer(ringBuffer, newElement);
+					ShowRingBuffer(ringBuffer);
 					break;
 				}
-
-				case 2:
+				else
 				{
-					bool answer = IsEmpty(ringBuffer);
-					if (answer == true)
-					{
-						printf("\nБуфер пуст!\n");
-					}
-					else
-					{
-						printf("\nБуфер не пуст!\n");
-						ShowRingBuffer(ringBuffer);
-					}
-					break;
-				}
-
-				case 3:
-				{
-					if (IfThereSpaceToWrite(ringBuffer))
-					{
-						int newElement;
-						EnterNumber("Введите значение нового элемента: ", newElement);
-						PushInRingBuffer(ringBuffer, newElement);
-						ShowRingBuffer(ringBuffer);
-						break;
-					}
-					else
-					{
-						printf("В буфере нет места для записи!\n");
-						ShowRingBuffer(ringBuffer);
-						break;
-					}
-
-				}
-
-				case 4:
-				{
-					int elementFromBuffer = PopFromRingBuffer(ringBuffer);
-					printf("Значение из буфера: %i\n", elementFromBuffer);
+					printf("В буфере нет места для записи!\n");
 					ShowRingBuffer(ringBuffer);
 					break;
 				}
 
-				case 5:
-				{
-					return;
-					break;
-				}
-
-				default:
-				{
-					printf("Попробуйте снова!\n");
-					break;
-				}
-
 			}
+
+			case 4:
+			{
+				int elementFromBuffer = PopFromRingBuffer(ringBuffer);
+				printf("Значение из буфера: %i\n", elementFromBuffer);
+				ShowRingBuffer(ringBuffer);
+				break;
+			}
+
+			case 5:
+			{
+				return;
+				break;
+			}
+
+			default:
+			{
+				printf("Попробуйте снова!\n");
+				break;
+			}
+
 		}
 	}
 }

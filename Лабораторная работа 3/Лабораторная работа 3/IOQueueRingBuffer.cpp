19b32d42dea98073bcb0eadfbe0686b89ed7623f
FloodDis <vlad.sechenov@mail.ru>
2022-02-18 16:33:12 +0700

    feature(IOQueueRingBuffer): убрал лишнее условие

--- Лабораторная работа 3/Лабораторная работа 3/IOQueueRingBuffer.cpp
+++ Лабораторная работа 3/Лабораторная работа 3/IOQueueRingBuffer.cpp
@@ -3,76 +3,79 @@
 void QueueRingBufferBasedMenu()
 {
 	setlocale(LC_ALL, "Russian");
-	int option;
 	int sizeOfQueue;
-	cout << "Введите размер очереди: ";
-	cin >> sizeOfQueue;
-	if ((int)log2(sizeOfQueue) % 2 != 0)
+	bool tryAgain = true;
+	while (tryAgain)
 	{
-		cout << "Размер буфера должен степенью 2!\n";
-		return;
-	}
-	else
-	{
-		QueueRingBufferBased* queueRingBuffer = new QueueRingBufferBased;
-		queueRingBuffer->ringBuffer = CreationOfQueue(sizeOfQueue);
-		cout << "Очередь создана!\n";
-		ShowQueueRingBuffer(queueRingBuffer->ringBuffer);
-		while (true)
+		try
 		{
-			cout << "Выберете вариант:\n1) Добавить элемент в очередь\n2) Извлечение элемента из очереди\n";
-			cout << "3) Удалить очередь\n4) Выход\n";
-			option = EnterNumber("");
-			switch (option)
+			sizeOfQueue = EnterNumber("Введите размер очереди: ");
+			tryAgain = false;
+		}
+		catch (const char* error)
+		{
+			cout << "Size must be a number!\n";
+		}
+	}
+	QueueRingBufferBased* queueRingBuffer = new QueueRingBufferBased;
+	queueRingBuffer->ringBuffer = CreationOfQueue(sizeOfQueue);
+	cout << "Очередь создана!\n";
+	ShowQueueRingBuffer(queueRingBuffer->ringBuffer);
+	while (true)
+	{
+		cout << "Выберете вариант:\n1) Добавить элемент в очередь\n2) Извлечение элемента из очереди\n";
+		cout << "3) Удалить очередь\n4) Выход\n";
+		int option;
+		option = EnterNumber("");
+		switch (option)
+		{
+			case 1:
 			{
-				case 1:
-				{
-					int newElement = EnterNumber(
-						"Введите новый элемент очереди\n");
-					EnqueueRingBuffer(queueRingBuffer->ringBuffer, newElement);
-					ShowQueueRingBuffer(queueRingBuffer->ringBuffer);
-					break;
-				}
+				int newElement = EnterNumber(
+					"Введите новый элемент очереди\n");
+				EnqueueRingBuffer(queueRingBuffer->ringBuffer, newElement);
+				ShowQueueRingBuffer(queueRingBuffer->ringBuffer);
+				break;
+			}
 
-				case 2:
-				{
-					int elementFromQueue;
-					elementFromQueue = DequeueRingBuffer(queueRingBuffer->ringBuffer);
-					cout << "Извлеченный элемент: %i\n", elementFromQueue;
-					ShowQueueRingBuffer(queueRingBuffer->ringBuffer);
-					break;
-				}
+			case 2:
+			{
+				int elementFromQueue;
+				elementFromQueue = DequeueRingBuffer(queueRingBuffer->ringBuffer);
+				cout << "Извлеченный элемент: %i\n", elementFromQueue;
+				ShowQueueRingBuffer(queueRingBuffer->ringBuffer);
+				break;
+			}
 
-				case 3:
+			case 3:
+			{
+				DeleteQueueRingBuffer(queueRingBuffer->ringBuffer);
+				cout << "Ваша очередь удалена!\n";
+				int sizeOfQueue;
+				EnterNumber("Введите размер очереди: ", sizeOfQueue);
+				if ((int)log2(sizeOfQueue) % 2 != 0)
 				{
-					DeleteQueueRingBuffer(queueRingBuffer->ringBuffer);
-					cout << "Ваша очередь удалена!\n";
-					int sizeOfQueue;
-					EnterNumber("Введите размер очереди: ", sizeOfQueue);
-					if ((int)log2(sizeOfQueue) % 2 != 0)
-					{
-						printf("Размер буфера должен быть степенью 2\n");
-						return;
-						break;
-					}
-					else
-					{
-						queueRingBuffer->ringBuffer = CreationOfQueue(sizeOfQueue);
-						cout << "Очередь создана!\n";
-						break;
-					}
-				}
-
-				case 4:
-				{
+					printf("Размер буфера должен быть степенью 2\n");
 					return;
-				}
-
-				default:
-				{
-					cout << "Попробуйте ещё раз!\n";
 					break;
 				}
+				else
+				{
+					queueRingBuffer->ringBuffer = CreationOfQueue(sizeOfQueue);
+					cout << "Очередь создана!\n";
+					break;
+				}
+			}
+
+			case 4:
+			{
+				return;
+			}
+
+			default:
+			{
+				cout << "Попробуйте ещё раз!\n";
+				break;
 			}
 		}
 	}

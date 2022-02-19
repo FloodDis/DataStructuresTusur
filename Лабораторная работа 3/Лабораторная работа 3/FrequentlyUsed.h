e93a444d4f9315d26cec9ac4fe88d0fd4f13d6e9
FloodDis <vlad.sechenov@mail.ru>
2022-02-19 10:28:45 +0700

    style(FrequentlyUsed): написал комментарий для метода EnterNumber

--- Лабораторная работа 3/Лабораторная работа 3/FrequentlyUsed.h
+++ Лабораторная работа 3/Лабораторная работа 3/FrequentlyUsed.h
@@ -2,4 +2,9 @@
 #include<iostream>
 using namespace std;
 
+/// <summary>
+/// Функция ввода переменной типа int и вывода сообщения на экран
+/// </summary>
+/// <param name="message">текст выводимого сообщения</param>
+/// <returns>вводимое значение</returns>
 int EnterNumber(string message);

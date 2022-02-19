646038473e27f57a71213eed8619f435c2375ae3
FloodDis <vlad.sechenov@mail.ru>
2022-02-19 10:12:46 +0700

    fix(IOQueueRingBuffer): исправил код с учетом измененного метода EnterNumber

--- Лабораторная работа 3/Лабораторная работа 3/IOQueueRingBuffer.cpp
+++ Лабораторная работа 3/Лабораторная работа 3/IOQueueRingBuffer.cpp
@@ -3,20 +3,7 @@
 void QueueRingBufferBasedMenu()
 {
 	setlocale(LC_ALL, "Russian");
-	int sizeOfQueue;
-	bool tryAgain = true;
-	while (tryAgain)
-	{
-		try
-		{
-			sizeOfQueue = EnterNumber("Введите размер очереди: ");
-			tryAgain = false;
-		}
-		catch (const char* error)
-		{
-			cout << "Size must be a number!\n";
-		}
-	}
+	int sizeOfQueue = EnterNumber("Введите размер очереди: ");
 	QueueRingBufferBased* queueRingBuffer = new QueueRingBufferBased;
 	queueRingBuffer->ringBuffer = CreationOfQueue(sizeOfQueue);
 	cout << "Очередь создана!\n";
@@ -26,44 +13,16 @@ void QueueRingBufferBasedMenu()
 		cout << "Выберете вариант:\n1) Добавить элемент в очередь\n2) Извлечение элемента из очереди\n";
 		cout << "3) Удалить очередь\n4) Выход\n";
 		int option;
-		bool tryAgain = true;
-		while (tryAgain)
+		do
 		{
-			try
-			{
-				option = EnterNumber("");
-				tryAgain = false;
-			}
-			catch (const char* error)
-			{
-				cout << "Option must be a number!\n";
-			}
-		}
-		
-		while (option < 1 || option>4)
-		{
-			cout << "Option must be 1-4!\n";
-			cin >> option;
-		}
-
+			option = EnterNumber("");
+		} while (option < 1 || option>4);
 		switch (option)
 		{
 			case 1:
 			{
-				int newElement;
-				bool tryAgain = true;
-				while (tryAgain)
-				{
-					try
-					{
-						newElement = EnterNumber(
-							"Введите новый элемент очереди\n");
-					}
-					catch (const char* error)
-					{
-						cout << "Element must be a number!\n";
-					}
-				}
+				int newElement = EnterNumber(
+					"Введите новый элемент очереди\n");
 				EnqueueRingBuffer(queueRingBuffer->ringBuffer, newElement);
 				ShowQueueRingBuffer(queueRingBuffer->ringBuffer);
 				break;
@@ -71,7 +30,7 @@ void QueueRingBufferBasedMenu()
 
 			case 2:
 			{
-				int elementFromQueue = 
+				int elementFromQueue =
 					DequeueRingBuffer(queueRingBuffer->ringBuffer);
 				cout << "Извлеченный элемент:" << elementFromQueue << "\n";
 				ShowQueueRingBuffer(queueRingBuffer->ringBuffer);
@@ -82,20 +41,10 @@ void QueueRingBufferBasedMenu()
 			{
 				DeleteQueueRingBuffer(queueRingBuffer->ringBuffer);
 				cout << "Ваша очередь удалена!\n";
-				int sizeOfQueue;
-				EnterNumber("Введите размер очереди: ", sizeOfQueue);
-				if ((int)log2(sizeOfQueue) % 2 != 0)
-				{
-					printf("Размер буфера должен быть степенью 2\n");
-					return;
-					break;
-				}
-				else
-				{
-					queueRingBuffer->ringBuffer = CreationOfQueue(sizeOfQueue);
-					cout << "Очередь создана!\n";
-					break;
-				}
+				int sizeOfQueue = EnterNumber("Введите размер очереди: ");
+				queueRingBuffer->ringBuffer = CreationOfQueue(sizeOfQueue);
+				cout << "Очередь создана!\n";
+				break;
 			}
 
 			case 4:

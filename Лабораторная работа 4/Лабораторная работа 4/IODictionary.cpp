ace5289a56aa57c3f0385eea7776c7f6e7799f26
FloodDis <vlad.sechenov@mail.ru>
2022-02-20 13:57:22 +0700

    Исправил код с учетом измененных методов EnterNumber и EnterString

--- Лабораторная работа 4/Лабораторная работа 4/IODictionary.cpp
+++ Лабораторная работа 4/Лабораторная работа 4/IODictionary.cpp
@@ -4,28 +4,30 @@ void DictionaryMenu()
 {
 	Dictionary* dictionaryUnit = new Dictionary;
 	setlocale(LC_ALL, "ru");
-	int size;
-	EnterNumber("Введите размер словаря:\n", size);
+	int size = EnterNumber("Введите размер словаря:\n");
 	InitializationOfDictionary(dictionaryUnit->HashTable, size);
 	ShowDictionary(dictionaryUnit->HashTable);
-	int option;
+
 	while (true)
 	{
 		printf("Выберите действие:\n1) Добавить элемент\n2) Удалить элемент\n3) Поиск элемента\n4) Рехешинг\n5) Выйти\n");
-		cin >> option;
+
+		int option = EnterNumber("");
 		switch (option)
 		{
 			case 1:
 			{
-				string value;
-				string key;
-				double size = dictionaryUnit->HashTable.size();
-				EnterString("Введите значение value: ", value);
-				EnterString("Введите значение key: ", key);
-				double fillFactor = ElementCount(dictionaryUnit->HashTable) / size;
-				if (fillFactor >= 0.9)
+				string value = EnterString("Введите значение value: ");
+				string key = EnterString("Введите значение key: ");
+				double size =
+					dictionaryUnit->HashTable->arrayOfLists.size();
+				double fillFactor =
+					ElementCount(dictionaryUnit->HashTable) / size;
+				if (fillFactor >= dictionaryUnit->HashTable->MaxFillFactor)
 				{
-					Rehashing(dictionaryUnit->HashTable, ElementCount(dictionaryUnit->HashTable));
+					int elementCount =
+						ElementCount(dictionaryUnit->HashTable);
+					Rehashing(dictionaryUnit->HashTable, elementCount);
 				}
 				AddInDictionary(dictionaryUnit->HashTable, value, key);
 				ShowDictionary(dictionaryUnit->HashTable);
@@ -34,8 +36,8 @@ void DictionaryMenu()
 
 			case 2:
 			{
-				string key;
-				EnterString("Введите key удаляемого элемента: ", key);
+				string key =
+					EnterString("Введите key удаляемого элемента: ");
 				DeleteFromDictionary(dictionaryUnit->HashTable, key);
 				ShowDictionary(dictionaryUnit->HashTable);
 				break;
@@ -43,18 +45,29 @@ void DictionaryMenu()
 
 			case 3:
 			{
-				string key;
-				string answer;
-				EnterString("Введите key искомого элемента: ", key);
-				answer = SearchInDictionary(dictionaryUnit->HashTable, key);
-				cout << answer;
-				ShowDictionary(dictionaryUnit->HashTable);
-				break;
+				string key = 
+					EnterString("Введите key искомого элемента: ");
+				string answer = "";
+				answer = 
+					SearchInDictionary(dictionaryUnit->HashTable, key);
+				if (answer == "")
+				{
+					cout << "Элемент не найден.\n";
+					ShowDictionary(dictionaryUnit->HashTable);
+					break;
+				}
+				else
+				{
+					cout << answer;
+					ShowDictionary(dictionaryUnit->HashTable);
+					break;
+				}
 			}
 
 			case 4:
 			{
-				Rehashing(dictionaryUnit->HashTable, ElementCount(dictionaryUnit->HashTable));
+				int elementCount = ElementCount(dictionaryUnit->HashTable);
+				Rehashing(dictionaryUnit->HashTable, elementCount);
 				ShowDictionary(dictionaryUnit->HashTable);
 				break;
 			}
@@ -62,12 +75,12 @@ void DictionaryMenu()
 			case 5:
 			{
 				return;
-				break;
 			}
 
 			default:
 			{
 				cout << "Попробуйте снова!" << endl;
+				ShowDictionary(dictionaryUnit->HashTable);
 				break;
 			}
 		}
